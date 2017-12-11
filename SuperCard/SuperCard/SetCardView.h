90aaf9183a5a9d302d9f8786eb6e648b20de7346
nimrod gruber <nimrod@lightricks.com>
2017-12-11 07:14:25 +0200

    Ex-4, task-1, creating set view

---
+++ SuperCard/SuperCard/SetCardView.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by nimrod gruber.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SetCardView : UIView
+
+- (BOOL)cardIsNotInitialized;
+- (void)pinch:(UIPinchGestureRecognizer *)gesture;
+- (void)updateCardDisplay:(NSString *)suit rank:(NSUInteger)rank;
+
+@property (nonatomic) NSUInteger rank;
+@property (strong, nonatomic) NSString *suit;
+@property (nonatomic) BOOL faceUp;
+
+@end
+
+NS_ASSUME_NONNULL_END
