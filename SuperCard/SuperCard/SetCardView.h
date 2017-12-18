f3849e0066b0580aa62c1a7e32dc4b8737f6f056
nimrod gruber <nimrod@lightricks.com>
2017-12-18 08:12:55 +0200

    ex-4, more refactoring and cleaning up

--- SuperCard/SuperCard/SetCardView.h
+++ SuperCard/SuperCard/SetCardView.h
@@ -5,19 +5,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SetCardView : UIView
 
-- (BOOL)cardIsNotInitialized;
 - (void)pinch:(UIPinchGestureRecognizer *)gesture;
 - (void)updateCardDisplay:(int)color theNumber:(int)number
                  theShade:(int)shade theSymbol:(int)symbol;
-
-@property (nonatomic) NSUInteger rank;
-@property (strong, nonatomic) NSString *suit;
 
 @property (nonatomic) int color;
 @property (nonatomic) int number;
 @property (nonatomic) int shading;
 @property (nonatomic) int symbol;
-
 @property (nonatomic) BOOL faceUp;
 
 @end
--- SuperCard/SuperCard/SetCardView.mm
+++ SuperCard/SuperCard/SetCardView.mm
@@ -32,23 +32,9 @@ NS_ASSUME_NONNULL_BEGIN
   [self setNeedsDisplay];
 }
 
-- (void)setSuit:(NSString *)suit {
-  _suit = suit;
-  [self setNeedsDisplay];
-}
-
-- (void)setRank:(NSUInteger)rank {
-  _rank = rank;
-  [self setNeedsDisplay];
-}
-
 - (void)setFaceUp:(BOOL)faceUp {
   _faceUp = faceUp;
   [self setNeedsDisplay];
-}
-
-- (NSString *)rankAsString {
-  return @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"][self.rank];
 }
 
 #pragma mark - Gesture Handling
@@ -98,9 +84,7 @@ static const float kVerticalIndentationStartPositionFactor = 0.1;
    UIImage *faceImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@", imageName]];
    
    if (faceImage) {
-     CGFloat myX = self.bounds.size.width * 0.1; //x-axis indentation of 10% of the cardView
-//     [self drawShapes:faceImage X:(CGFloat)myX];
-     
+     CGFloat myX = self.bounds.size.width * 0.1;
      if (self.number == 1) {
        [self displayOneForm:faceImage X:(CGFloat)myX];
      } else if (self.number == 2) {
@@ -110,18 +94,6 @@ static const float kVerticalIndentationStartPositionFactor = 0.1;
      }
    }
 }
-
-//- (void)drawShapes:faceImage X:(CGFloat)myX {
-//  CGFloat myY = self.bounds.size.height * kVerticalIndentationStartPositionFactor;
-//
-//  for (int i = 0; i < self.number; ++i) {
-//    CGRect imageRect = CGRectMake(myX, myY, self.bounds.size.width * kHorizontalCompressionFactor,
-//                                  self.bounds.size.height * kVerticalCompressionFactror);
-//    [faceImage drawInRect:imageRect];
-//    myY = self.bounds.size.height * (kVerticalIndentationStartPositionFactor +
-//                                     (float)((i+1.0)/self.number) - (0.03 * (i+1)));
-//  }
-//}
 
 - (void)displayOneForm:faceImage X:(CGFloat)myX {
   CGFloat myY = self.bounds.size.height * 0.4;
@@ -210,107 +182,7 @@ static const float kVerticalIndentationStartPositionFactor = 0.1;
 
 #pragma  mark - Pips
 
-#define PIP_HOFFSET_PRECENTAGE 0.165
-#define PIP_VOFFSET1_PERCENTAGE 0.090
-#define PIP_VOFFSET2_PERCENTAGE 0.175
-#define PIP_VOFFSET3_PERCENTAGE 0.270
-
-- (void)drawPips {
-  if ((self.rank == 1) || (self.rank == 5) || (self.rank == 9) || (self.rank == 3)) {
-    [self drawPipsWithHorizontalOffset:0 verticalOffset:0 mirroredVertically:NO];
-  }
-  
-  if ((self.rank == 6) || (self.rank == 7) || (self.rank == 8)) {
-    [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PRECENTAGE verticalOffset:0 mirroredVertically:NO];
-  }
-  
-  if ((self.rank == 2) || (self.rank == 3) || (self.rank == 7) || (self.rank == 8) ||
-      (self.rank == 10)) {
-    [self drawPipsWithHorizontalOffset:0 verticalOffset:PIP_VOFFSET2_PERCENTAGE
-                    mirroredVertically:(self.rank != 7)];
-  }
-  
-  if ((self.rank ==4) || (self.rank ==5) || (self.rank == 6) || (self.rank == 7) ||
-      (self.rank == 8) || (self.rank == 9) || (self.rank == 10)) {
-    [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PRECENTAGE
-                        verticalOffset:PIP_VOFFSET3_PERCENTAGE mirroredVertically:YES];
-  }
-  
-  if ((self.rank ==9) || (self.rank == 10)) {
-    [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PRECENTAGE
-                        verticalOffset:PIP_VOFFSET1_PERCENTAGE mirroredVertically:YES];
-  }
-}
-
-#define PIP_FONT_SCALE_FACTOR 0.012
-
-- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
-                      verticalOffset:(CGFloat)voffset
-                          upsideDown:(BOOL)upsideDown {
-  if (upsideDown) {
-    [self pushContextAndRotateUpsideDown];
-  }
-  CGPoint middle = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
-  UIFont *pipFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-  pipFont = [pipFont fontWithSize:[pipFont pointSize] * self.bounds.size.width * PIP_FONT_SCALE_FACTOR];
-  NSAttributedString *attributedSuit = [[NSAttributedString alloc] initWithString:self.suit attributes:@{NSFontAttributeName : pipFont }];
-  CGSize pipSize = [attributedSuit size];
-  CGPoint pipOrigin = CGPointMake(middle.x - pipSize.width / 2.0 - hoffset * self.bounds.size.width,
-                                  middle.y - pipSize.height / 2.0 - voffset * self.bounds.size.height);
-  [attributedSuit drawAtPoint:pipOrigin];
-  if (hoffset) {
-    pipOrigin.x += hoffset * 2.0 * self.bounds.size.width;
-    [attributedSuit drawAtPoint:pipOrigin];
-  }
-  if (upsideDown) {
-    [self popContext];
-  }
-}
-
--(void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
-                     verticalOffset:(CGFloat)voffset
-                 mirroredVertically:(BOOL)mirroredVertically {
-  [self drawPipsWithHorizontalOffset:hoffset
-                      verticalOffset:voffset
-                          upsideDown:NO];
-  if (mirroredVertically) {
-    [self drawPipsWithHorizontalOffset:hoffset
-                        verticalOffset:voffset
-                            upsideDown:YES];
-  }
-}
-
-- (void)pushContextAndRotateUpsideDown {
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  CGContextSaveGState(context);
-  CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
-  CGContextRotateCTM(context, M_PI);
-}
-
-- (void)popContext {
-  CGContextRestoreGState(UIGraphicsGetCurrentContext());
-}
-
 #pragma mark - Corners
-
-- (void)drawCorners {
-//  NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-//  paragraphStyle.alignment = NSTextAlignmentCenter;
-//
-//  UIFont *cornerFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-//  cornerFont = [cornerFont fontWithSize:cornerFont.pointSize * [self cornerScaleFactor]];
-//
-//  NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n%@", [self rankAsString], self.suit] attributes:@{NSFontAttributeName : cornerFont, NSParagraphStyleAttributeName : paragraphStyle }];
-//
-//  CGRect textBounds;
-//  textBounds.origin = CGPointMake([self cornerOffset], [self cornerOffset]);
-//  textBounds.size = [cornerText size];
-//  [cornerText drawInRect:textBounds];
-//
-//  [self pushContextAndRotateUpsideDown];
-//  [cornerText drawInRect:textBounds];
-//  [self popContext];
-}
 
 #pragma mark - Initialization
 
@@ -333,14 +205,6 @@ static const float kVerticalIndentationStartPositionFactor = 0.1;
 
 #pragma mark - Personal Utility functions
 
-- (BOOL)cardIsNotInitialized {
-  if (self.color == undefinedColor) {
-    return YES;
-  } else {
-    return NO;
-  }
-}
-
 - (void)updateCardDisplay:(int)color theNumber:(int)number theShade:(int)shade
       theSymbol:(int)symbol {
   self.color = color;
@@ -350,6 +214,5 @@ static const float kVerticalIndentationStartPositionFactor = 0.1;
 }
 
 @end
-
 
 NS_ASSUME_NONNULL_END
